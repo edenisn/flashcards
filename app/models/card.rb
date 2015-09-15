4f2de573525b94a8953f3aae478d07d618c92a53
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-16 01:13:23 +0300

    Add CRUD operations to CardsController & add validators to model Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,9 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  validate :original_text_cannot_be_equal_translated_text
+
+  def original_text_cannot_be_equal_translated_text
+    errors.add(:translated_text, "Переведенный текст не должен совпадать с оригиналом!") if original_text.casecmp(translated_text) == 0
+  end
 end
