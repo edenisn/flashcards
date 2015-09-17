0a0097367dc6717a99b27968eb42e56cc3e65825
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-17 17:41:25 +0300

    review code seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-arr_original = [] # array for text (original_text[text])
-arr_translated = [] # array for text (translated_text[text])
-
 PAGE_URL = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 
 page = Nokogiri::HTML(open(PAGE_URL))
 
-cards_original_text = page.css("table tr td.bigLetter")
-cards_original_text.each { |t| arr_original << t.text }
-
-cards_translated_text = page.css("table tr td.bigLetter + td")
-cards_translated_text.each { |t| arr_translated << t.text }
-
-cards_list = arr_original.zip(arr_translated) # cards_list = [ arr_original, arr_translated ]
-
-cards_list.each do |original, translate|
-  Card.create(original_text: original, translated_text: translate, review_date: DateTime.now)
+page.css("table tr").each do |tr|
+  original_text = tr.css("td.bigLetter").text
+  translated_text = tr.css("td.bigLetter + td").text
+  Card.create(original_text: original_text, translated_text: translated_text)
 end
 
