7c02ce302a6afb6405a3d0138ab3695e02dbc488
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-18 14:02:34 +0300

    add random card to main page & possibility to verify translated text

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @card = Card.created_before(DateTime.now).random.first
+  end
+
+  def examine
+    @card = Card.find(params[:id])
+
+    if @card.verify_translate(params[:original], params[:translated])
+      @card.update_attributes(review_date: params[:date])
+      redirect_to :back, notice: "Правильно"
+    else
+      redirect_to :back, notice: "Не правильно!"
+    end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,12 +1,31 @@
 require 'active_support/all'
 
 class Card < ActiveRecord::Base
+  scope :created_before, -> time { where("review_date <= ?", time) }
+  scope :random, -> { order("RANDOM()") }
 
   before_update :add_3_days_to_review_date_after_update
 
   validates :original_text, :translated_text, :review_date, presence: { message: 'Поле не может быть пустым' }
 
   validate :original_text_cannot_be_equal_translated_text
+
+  def self.created_before(time)
+    where("review_date <= ?", time)
+  end
+
+  def self.random
+    order("RANDOM()") # get random row from postgresql
+  end
+
+  def verify_translate(original, translated)
+    if original.mb_chars == translated.mb_chars
+      add_3_days_to_review_date_after_update
+      true
+    else
+      false
+    end
+  end
 
   private
     def original_text_cannot_be_equal_translated_text
