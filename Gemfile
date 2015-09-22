1d18ea7fe0027ff4bb18e2f5ef69860ad721a907
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-22 10:06:50 +0300

    add RSpec tests for model Card

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-group :development, :test do
+group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   #gem 'byebug'
   gem 'pry-byebug', platform: [:ruby_20]
@@ -45,5 +45,10 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :test do
+  gem 'rspec-rails', '~>3.0.0'
+  gem 'factory_girl_rails'
 end
 
---
+++ spec/features/card_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+describe Card do
+
+  let!(:card) { create(:card) }
+
+  context "aliased attributes" do
+
+    it "is the correct original_text card" do
+        expect(card.original_text).to eq "Fruit"
+    end
+
+    it "is the correct translated_text card" do
+        expect(card.translated_text).to eq "Фрукт"
+    end
+
+    it "is not the correct translated_text card" do
+        expect(card.translated_text).to_not eq "Кран"
+    end
+
+    it "is the correct review_date card" do
+        expect(card.review_date).to eq(Date.today + 3.days)
+    end
+
+  end
+
+  context "not valid object" do
+
+    before(:each) do
+      @card1 = Card.create(original_text: 'house', translated_text: 'house')
+      @card2 = Card.create(original_text: 'house', translated_text: 'HOuSe')
+    end
+
+    it "is not return valid object if original_text equal translated_text" do
+      expect(@card1).to_not be_valid
+    end
+
+    it "is not return valid object if original_text equal translated_text
+         even with small and lower-case letters" do
+      expect(@card2).to_not be_valid
+    end
+
+  end
+
+  context "scopes" do
+
+    it "is not include card that have review_date >= Date.today" do
+      expect(Card.for_review).to_not include(card)
+    end
+
+    it "is include card that have review_date <= Date.today" do
+      card.update_attributes(review_date: Date.today)
+      expect(Card.for_review).to include(card)
+    end
+
+  end
+
+  context "methods" do
+
+    it "is false if translation is not correct" do
+      expect(card.verify_translation("Banana")).to be false
+    end
+
+    it "is true if translation is correct" do
+      expect(card.verify_translation("Fruit")).to be true
+    end
+
+    it "is true if translation is correct even with small and lower-case letters" do
+      expect(card.verify_translation("fRUiT")).to be true
+    end
+
+  end
+
+end
