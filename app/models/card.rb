20d8d23e0ff797a7209e59ccf0aa145a17ed5c6e
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-28 20:42:11 +0300

    add gem paperclip for file attachment to Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,11 +3,19 @@ class Card < ActiveRecord::Base
 
   scope :for_review, -> { where("review_date <= ?", Date.today).order("RANDOM()") }
 
+  has_attached_file :image, styles: {
+    thumb: "100x100>",
+    large: "360x360#"
+  }
+
   before_create :set_default_review_date
 
   validates :original_text, :translated_text, :review_date, presence: { message: 'Поле не может быть пустым' }
 
   validate :original_text_cannot_be_equal_translated_text
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 
   def verify_translation(user_translation)
     if transform_string(original_text) == transform_string(user_translation)
