f7be1705d823617e7e0d9cc43da4f921a11cb8f4
Denis Erofeev <erofeevdn@yandex.ru>
2015-09-29 11:13:08 +0300

    DRY model User with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,14 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :authentications
 
-  validates :password, length: { minimum: 6, message: "Длина пароля должна быть не меньше 6 символов" },
-            if: -> { new_record? || changes["password"] }
-  validates :password, confirmation: { message: "Пароли не совпадают" },
-            if: -> { new_record? || changes["password"] }
-  validates :password_confirmation, presence: { message: "Поле не может быть пустым" },
-            if: -> { new_record? || changes["password"] }
-
   validates :email, uniqueness: { message: 'Такой email уже существует' }, presence: { message: "Поле не может быть пустым" },
             :format => { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: 'Введите правильный формат email: username@host.com' }
+
+  with_options if: :new_record? || changes["password"] do |user|
+    user.validates :password, length: { minimum: 6, message: "Длина пароля должна быть не меньше 6 символов" }
+    user.validates :password, confirmation: { message: "Пароли не совпадают" }
+    user.validates :password_confirmation, presence: { message: "Поле не может быть пустым" }
+  end
 
   def has_linked_github?
     authentications.where(provider: 'github').present?
